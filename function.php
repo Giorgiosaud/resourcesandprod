3d8a6666520922239bf0bb58bc4b8573a04a1a8f
Jorge Saud <jorgelsaud@gmail.com>
2017-06-02 09:34:54 -0400

    fix funtcions.php

--- function.php
+++ function.php
@@ -1,12 +1,17 @@
 <?php
-function my_theme_enqueue_styles() {
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+function my_theme_enqueue_styles()
+{
+    $parentStyle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    wp_enqueue_style( $parentStyle, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array( $parentStyle ),
         wp_get_theme()->get('Version')
     );
-    wp_enqueue_style('custom-css',get_stylesheet_directory_uri().'/resources/css/custom.css',array($parent_style),wp_get_theme()->get('Version'));
+    wp_enqueue_style('custom-css',
+      get_stylesheet_directory_uri().'/resources/css/custom.css',
+      array(), '1.0'
+    );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
