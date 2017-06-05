f207f4088c022f658a037c1cc5a3225d553deffe
Jorge Saud <jorgelsaud@gmail.com>
2017-06-05 16:48:10 -0400

    update

--- admin/admin_initialize.php
+++ admin/admin_initialize.php
@@ -45,7 +45,7 @@ class MySettingsPage
             <?php
                 // This prints out all hidden setting fields
                 settings_fields( 'child_theme_group' );
-                do_settings_sections( 'my-setting-admin' );
+                do_settings_sections( 'child_theme_option' );
                 submit_button();
             ?>
             </form>
@@ -65,34 +65,34 @@ class MySettingsPage
         );
 
         add_settings_section(
-            'setting_section_id', // ID
+            'header_options', // ID
             'My Custom Settings', // Title
             array( $this, 'print_section_info' ), // Callback
-            'my-setting-admin' // Page
+            'child_theme_option' // Page
         );  
 
         add_settings_field(
             'imagen_top', // ID
             'Imagen Top', // Title 
             array( $this, 'imagen_top_callback' ), // Callback
-            'my-setting-admin', // Page
-            'setting_section_id' // Section           
+            'child_theme_option', // Page
+            'header_options' // Section           
         );
-        add_settings_field(
-            'id_number', // ID
-            'ID Number', // Title 
-            array( $this, 'id_number_callback' ), // Callback
-            'my-setting-admin', // Page
-            'setting_section_id' // Section           
-        );      
+        // add_settings_field(
+        //     'id_number', // ID
+        //     'ID Number', // Title 
+        //     array( $this, 'id_number_callback' ), // Callback
+        //     'my-setting-admin', // Page
+        //     'setting_section_id' // Section           
+        // );      
 
-        add_settings_field(
-            'title', 
-            'Title', 
-            array( $this, 'title_callback' ), 
-            'my-setting-admin', 
-            'setting_section_id'
-        );      
+        // add_settings_field(
+        //     'title', 
+        //     'Title', 
+        //     array( $this, 'title_callback' ), 
+        //     'my-setting-admin', 
+        //     'setting_section_id'
+        // );      
     }
 
     /**
@@ -103,11 +103,11 @@ class MySettingsPage
     public function sanitize( $input )
     {
         $new_input = array();
-        if( isset( $input['id_number'] ) )
-            $new_input['id_number'] = absint( $input['id_number'] );
+        // if( isset( $input['id_number'] ) )
+        //     $new_input['id_number'] = absint( $input['id_number'] );
 
-        if( isset( $input['title'] ) )
-            $new_input['title'] = sanitize_text_field( $input['title'] );
+        // if( isset( $input['title'] ) )
+        //     $new_input['title'] = sanitize_text_field( $input['title'] );
         if( isset( $input['imagen_top'] ) )
             $new_input['imagen_top'] = sanitize_text_field( $input['imagen_top'] );
 
@@ -125,24 +125,30 @@ class MySettingsPage
     /** 
      * Get the settings option array and print one of its values
      */
-    public function id_number_callback()
-    {
-        printf(
-            '<input type="text" id="id_number" name="child_theme[id_number]" value="%s" />',
-            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+    public function imagen_top_callback(){
+    	printf(
+            '<input type="text" id="imagen_top" name="child_theme[imagen_top]" value="%s" />',
+            isset( $this->options['imagen_top'] ) ? esc_attr( $this->options['imagen_top']) : ''
         );
     }
+    // public function id_number_callback()
+    // {
+    //     printf(
+    //         '<input type="text" id="id_number" name="child_theme[id_number]" value="%s" />',
+    //         isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+    //     );
+    // }
 
     /** 
      * Get the settings option array and print one of its values
      */
-    public function title_callback()
-    {
-        printf(
-            '<input type="text" id="title" name="child_theme[title]" value="%s" />',
-            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
-        );
-    }
+    // public function title_callback()
+    // {
+    //     printf(
+    //         '<input type="text" id="title" name="child_theme[title]" value="%s" />',
+    //         isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+    //     );
+    // }
 }
 
 if( is_admin() )
