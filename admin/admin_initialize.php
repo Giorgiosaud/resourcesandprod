bc3e9a015fa28e39484b54e40cc35f4b10791a7c
Jorge Saud <jorgelsaud@gmail.com>
2017-06-05 09:19:51 -0400

    add child options page

--- admin/admin_initialize.php
+++ admin/admin_initialize.php
@@ -1,7 +1,7 @@
 <?php
 add_action('admin_menu', 'child_theme_menu');
 
-function tw_theme_menu() {
+function child_theme_menu() {
 	add_theme_page('Zonapro Options', 'Zonapro Child Theme Option', 'edit_theme_options', 'child_theme_option', 'child_options');
 }
 function child_options()
