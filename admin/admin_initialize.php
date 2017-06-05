52d4c8f60629e739a9700bb3ff768e3990db588c
Jorge Saud <jorgelsaud@gmail.com>
2017-06-05 10:21:33 -0400

    test

--- admin/admin_initialize.php
+++ admin/admin_initialize.php
@@ -1,11 +1,9 @@
 <?php
 add_action('admin_menu', 'child_theme_menu');
-
 function child_theme_menu() {
 	add_theme_page('Zonapro Options', 'Zonapro Child Theme Option', 'edit_theme_options', 'child_theme_option', 'child_options');
 }
 function child_options()
 {
-	echo 'Hola';
-	//require_once CHILD_ADMIN_DIR.'/script/options_settings.php';
+	echo require CHILD_ADMIN_DIR.'/Page/child_options_settings.php';
 }
