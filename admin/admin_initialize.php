4dbfee85af02ae314a0d42a79b6f2a30b6e1c523
Jorge Saud <jorgelsaud@gmail.com>
2017-06-05 09:17:44 -0400

    adding a customizer options page to child theme

---
+++ admin/admin_initialize.php
@@ -0,0 +1,11 @@
+<?php
+add_action('admin_menu', 'child_theme_menu');
+
+function tw_theme_menu() {
+	add_theme_page('Zonapro Options', 'Zonapro Child Theme Option', 'edit_theme_options', 'child_theme_option', 'child_options');
+}
+function child_options()
+{
+	echo 'Hola';
+	//require_once CHILD_ADMIN_DIR.'/script/options_settings.php';
+}
