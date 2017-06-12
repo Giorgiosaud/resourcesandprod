76bccd92a8477641cce5f85553ab337272628236
Jorge Saud <jorgelsaud@gmail.com>
2017-06-11 21:22:59 -0400

    fix shortcode services;

--- functions.php
+++ functions.php
@@ -646,3 +646,85 @@ function save_service_info_child($post_ID)
     endif;
 }
 
+
+
+
+
+
+add_action('init', 'remove_parent_theme_shortcodes');
+
+function remove_parent_theme_shortcodes() {
+
+
+// remove shortcode from parent theme
+
+// shortcode_name should be the name of the shortcode you want to modify
+
+remove_shortcode( 'doors-service' );
+
+
+// add the same shortcode in child theme with our own function
+
+// note to self: think of a better example function name
+
+add_shortcode( 'doors-service', 'service_shortcode_child' );
+
+}
+
+
+
+function service_shortcode_child($atts) {
+    extract(shortcode_atts(array(
+        'category' => '',
+        'type' => '',
+        'sitem' => ''
+                    ), $atts, 'wishlist'));
+
+
+    $service_return = '';
+
+    $service_return .= '<div class="container"><div class="row">';
+
+    
+    
+    $q = new WP_Query(
+            array('post_type' => array('service'),
+        'post_status' => array('publish'),
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'posts_per_page' => $sitem,
+        'service-category' => $category)
+    );
+
+
+    $service_return = '<div class="item active">';
+    while ($q->have_posts()) : $q->the_post();
+        //$idz = get_the_ID();
+        $icon = get_post_meta(get_the_ID(), 'serviceIcon_child', true);
+        if ($icon != '' && $icon != 1) {
+            $ico = 'fa ' . $icon;
+        } else {
+            $ico = 'fa fa-bomb';
+        }
+        $service_return .= '
+               
+              <div class="col-md-3 col-sm-6 wow zoomIn text-center service-item" data-wow-duration="700ms" data-wow-delay="300ms">          
+                                    <div class="service-icon">
+                                            <i class="' . $ico . '"></i>              
+                                    </div>
+                                    <div class="service-text">
+                                            <h4>' . get_the_title() . '</h4>
+                                            <p>' .get_the_content(). '</p>
+                                    </div>          
+                            </div>
+               
+                ';
+    endwhile;
+    $service_return.= '</div>';
+    wp_reset_query();
+    $service_return .= '</div>';
+
+    return $service_return;
+}
+
+add_shortcode('doors-service', 'service_shortcode');
