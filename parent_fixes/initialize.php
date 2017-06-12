23a6da392792b6329f781aac9392a1fadb1786ad
Jorge Saud <jorgelsaud@gmail.com>
2017-06-12 08:50:01 -0400

    zp-team shortcode

--- parent_fixes/initialize.php
+++ parent_fixes/initialize.php
@@ -1,4 +1,5 @@
 <?php
 require 'team/add_team_excerpt.php';
+require 'team/add_team_shortcode.php';
 require 'services/add_my_own_metabox_icons.php';
 require 'services/add_zonapro_service_shortcode.php';
