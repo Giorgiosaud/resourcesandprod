de6d3e614e8cdeb4d38047b94e8177dfdba1ba8f
Jorge Saud <jorgelsaud@gmail.com>
2017-06-12 08:40:35 -0400

    reorder some fixes

--- parent_fixes/initialize.php
+++ parent_fixes/initialize.php
@@ -1,4 +1,4 @@
 <?php
 require 'team/add_team_excerpt.php';
 require 'team/add_my_own_metabox_icons.php';
-require 'team/add_zonapro_service_shortcode.php'
+require 'team/add_zonapro_service_shortcode.php';
