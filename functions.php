0e1ed65ca32539e92bd0ac82c3f12e1ff426d35e
Jorge Saud <jorgelsaud@gmail.com>
2017-06-11 21:13:07 -0400

    fix metabox in service icon

--- functions.php
+++ functions.php
@@ -37,45 +37,9 @@ function my_theme_enqueue_styles()
 
 
 
-/**
- * Edit/remove meta boxes already registered by a parent theme / plugin
- * Priority 20 makes sure all meta boxes are already registered
- */
-add_filter( 'rwmb_meta_boxes', 'prefix_edit_meta_boxes', 20 );
-function prefix_edit_meta_boxes( $meta_boxes )
-{
-  // Loop throught all meta boxes to find the ones we need
-  foreach ( $meta_boxes as $k => $meta_box )
-  {
-    var_dump($meta_boxes);
-    // Remove "Personal Information" meta box
-    if ( isset( $meta_box['id'] ) && 'service_icon_info' == $meta_box['id'] )
-    {
-      unset( $meta_boxes[$k] );
-    }
-    // Edit "Address Info" meta box
-    // if ( isset( $meta_box['id'] ) && 'address' == $meta_box['id'] )
-    // {
-      // Change title to "Address"
-      // $meta_boxes[$k]['title'] = 'Address';
-      // Loop through all fields
-      // foreach ( $meta_box['fields'] as $j => $field )
-      // {
-        // Add description for "Street" field
-        // if ( 'your_prefix_street' == $field['id'] )
-        // {
-          // $meta_boxes[$k][$j]['desc'] = 'Enter street adress';
-        // }
-      // }
-      // Add field "zip_code" to this meta box
-      // $meta_boxes[$k]['fields'][] = array(
-        // 'name' => 'Zip code',
-        // 'id'   => 'zip_code',
-        // 'type' => 'text',
-      // );
-    // }
-  }
-  // Return edited meta boxes
-  return $meta_boxes;
+add_action( 'admin_menu', 'my_remove_meta_boxes' );
+function my_remove_meta_boxes() {
+  // if ( ! current_user_can( 'manage_options' ) ) {
+    remove_meta_box('team_exInfo', 'service', 'side' );
+  // }
 }
-
