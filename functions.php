c5ca58dc4b18326c9e051f55c1c7d3730ab36d55
Jorge Saud <jorgelsaud@gmail.com>
2017-06-11 20:58:53 -0400

    fix metabox in service icon

--- functions.php
+++ functions.php
@@ -31,9 +31,50 @@ function my_theme_enqueue_styles()
   }
   add_action( 'init', 'team_add_excerpt' ,20);
   add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' ,11);
-function wpse59607_remove_meta_box( $callback )
+
+
+
+
+
+
+/**
+ * Edit/remove meta boxes already registered by a parent theme / plugin
+ * Priority 20 makes sure all meta boxes are already registered
+ */
+add_filter( 'rwmb_meta_boxes', 'prefix_edit_meta_boxes', 20 );
+function prefix_edit_meta_boxes( $meta_boxes )
 {
-    remove_meta_box('team_exInfo', __('Icon Info', 'wt'), 'service_icon_info', 'service', 'side', 'high' );
+  // Loop throught all meta boxes to find the ones we need
+  foreach ( $meta_boxes as $k => $meta_box )
+  {
+    // Remove "Personal Information" meta box
+    if ( isset( $meta_box['id'] ) && 'team_exInfo' == $meta_box['id'] )
+    {
+      unset( $meta_boxes[$k] );
+    }
+    // Edit "Address Info" meta box
+    // if ( isset( $meta_box['id'] ) && 'address' == $meta_box['id'] )
+    // {
+      // Change title to "Address"
+      // $meta_boxes[$k]['title'] = 'Address';
+      // Loop through all fields
+      // foreach ( $meta_box['fields'] as $j => $field )
+      // {
+        // Add description for "Street" field
+        // if ( 'your_prefix_street' == $field['id'] )
+        // {
+          // $meta_boxes[$k][$j]['desc'] = 'Enter street adress';
+        // }
+      // }
+      // Add field "zip_code" to this meta box
+      // $meta_boxes[$k]['fields'][] = array(
+        // 'name' => 'Zip code',
+        // 'id'   => 'zip_code',
+        // 'type' => 'text',
+      // );
+    // }
+  }
+  // Return edited meta boxes
+  return $meta_boxes;
 }
-add_action( 'add_meta_boxes_page', 'wpse59607_remove_meta_box', 999 );
 
