009969de9a7c0b83898698706d485cf9691a98e9
Jorge Saud <jorgelsaud@gmail.com>
2017-06-14 16:44:38 -0400

    autoload

---
+++ parent_fixes/contact/contactDetails.php
@@ -0,0 +1,55 @@
+<?php
+function contactdetails_child($atts)
+{
+    extract(shortcode_atts(array(
+        'title'          => 'Visit Our Office',
+        'address'          => '',
+        'phone'         => '',
+        'email'         => '',
+
+        ), $atts));
+    $tel = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+    $contact_address_return = '<div class="col-sm-4 wow zoomIn contactdetails" data-wow-duration="700ms" data-wow-delay="300ms">';
+
+    $contact_address_return .= '<h2>'.$title.'</h2>';
+    $contact_address_return .= '<address>';
+    if($address!=''){
+        $contact_address_return .= "<p><i class='fa fa-map-marker'></i> Address: $address</p>";
+    }
+    if($phone!=''){
+        $contact_address_return .= "<p><i class='fa fa-phone'></i> <a href='tel:$tel'>Telefono: $phone </a></p>";
+    }
+    $contact_address_return .= "<p><i class='fa fa-envelope'></i> Email: $email</p>";
+    $contact_address_return .= '</address>';
+    $contact_address_return .= '</div>';
+
+    return $contact_address_return;
+}
+add_shortcode( "tw-contactdetails", "contactdetails" );
+
+
+function contactdetails_child2($atts)
+{
+    extract(shortcode_atts(array(
+        'title'          => 'Visit Our Office',
+        'address'          => '',
+        'phone'         => '',
+        'email'         => '',
+
+        ), $atts));
+    
+    $contact_address_return = '<div class="col-sm-6 wow zoomIn contactdetails" data-wow-duration="700ms" data-wow-delay="300ms">';
+    $contact_address_return .= "<h2>$title</h2>";
+    $contact_address_return .= '<address>';
+    if($address!='')
+        $contact_address_return .= "<p><i class='fa fa-map-marker'></i> Address: $address</p>";
+    if($phone!='')
+        $contact_address_return .= "<p><i class='fa fa-phone'></i> <a href='tel:$tel'>Telefono: $phone </a></p>";
+    if($email!='')
+    $contact_address_return .= "<p><i class='fa fa-envelope'></i> Email: <a href='mailto:$email'> $email</a></p>";
+    $contact_address_return .= '</address>';
+    $contact_address_return .= '</div>';
+
+    return $contact_address_return;
+}
+add_shortcode( "zp-contactdetails2", "contactdetails_child2" );
--- parent_fixes/initialize.php
+++ parent_fixes/initialize.php
@@ -3,3 +3,5 @@ require 'team/add_team_excerpt.php';
 require 'team/add_team_shortcode.php';
 require 'services/add_my_own_metabox_icons.php';
 require 'services/add_zonapro_service_shortcode.php';
+require 'contact/contactDetails.php';
+require 'contact/contactForm.php';
