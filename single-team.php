ec34957c80f025527b11752b670731a29c5fdeda
Jorge Saud <jorgelsaud@gmail.com>
2017-06-28 16:14:19 -0400

    fix team single

--- single-team.php
+++ single-team.php
@@ -17,7 +17,7 @@ get_header(); ?>
                     // Start the Loop.
                     while ( have_posts() ) : the_post();
                         get_template_part( 'content', 'team' );
-                        if(get_option('commentswithc', false) != 'commentOff'):
+                        // if(get_option('commentswithc', false) != 'commentOff'):
                         if ( comments_open() || get_comments_number() ) 
                         {
                             comments_template();
