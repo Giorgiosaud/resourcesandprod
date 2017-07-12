2b04a3bee555bda1faebb880745a740be256cac8
Jorge Saud <jorgelsaud@gmail.com>
2017-07-12 11:31:24 -0400

    fix team single

--- parent_fixes/contact/contactDetails.php
+++ parent_fixes/contact/contactDetails.php
@@ -32,21 +32,31 @@ function contactdetails_child2($atts)
     extract(shortcode_atts(array(
         'title'          => 'Visit Our Office',
         'address'          => '',
-        'phone'         => '',
-        'email'         => '',
+        'phones'         => '',
+        'emails'        =>'',
 
         ), $atts));
-    $tel = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
-    
+    $phones=explode(',',$phones);
+    $emails=explode(',',$emails);
+    $tels=array();
+    foreach ($phones as $telephone) {
+        array_push($tels,filter_var($telephone,FILTER_SANITIZE_NUMBER_INT));
+    }
     $contact_address_return = '<div class="col-sm-6 wow zoomIn contactdetails" data-wow-duration="700ms" data-wow-delay="300ms">';
     $contact_address_return .= "<h2>$title</h2>";
     $contact_address_return .= '<address>';
     if($address!='')
         $contact_address_return .= "<p><i class='fa fa-map-marker'></i>$address</p>";
-    if($phone!='')
-        $contact_address_return .= "<p><i class='fa fa-phone'></i> <a href='tel:$tel'> $phone </a></p>";
-    if($email!='')
-        $contact_address_return .= "<p><i class='fa fa-envelope'></i><a href='mailto:$email'> $email</a></p>";
+    if(sizeof($phones)>1){
+        foreach ($phones as $key => $telephone) {
+            $contact_address_return .= "<p><i class='fa fa-phone'></i> <a href='tel:$tels[$key]'> $telephone </a></p>";
+        }
+    }
+    if(sizeof($emails)>1){
+        foreach ($emails as $email) {
+            $contact_address_return .= "<p><i class='fa fa-envelope'></i><a href='mailto:$email'> $email</a></p>";
+        }
+    }
     $contact_address_return .= '</address>';
     $contact_address_return .= '</div>';
 
